2cb614d59e7a54394747a9db3b874cc32217f7c2
futsuuu <105504350+futsuuu@users.noreply.github.com>
2025-09-29 01:55:33 +0900

    feat: add some basic type definitions for binary format (#3)
    probably enough to encode `(module (func))`

---
+++ src/binary.rs
@@ -0,0 +1,36 @@
+pub mod instr;
+pub mod ty;
+pub mod value;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Module {
+    pub func_sections: Vec<FuncSection>,
+    pub type_sections: Vec<TypeSection>,
+    pub code_sections: Vec<CodeSection>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Index<T>(pub u32, std::marker::PhantomData<T>);
+pub type TypeIndex = Index<ty::Recursive>;
+pub type LocalIndex = Index<Local>;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TypeSection(pub Vec<ty::Recursive>);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct FuncSection(pub Vec<TypeIndex>);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct CodeSection(pub Vec<Func>);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Func {
+    pub locals: Vec<Local>,
+    pub expr: instr::Expression,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Local {
+    pub num: u32,
+    pub ty: ty::Value,
+}
---
+++ src/binary/ty.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Recursive(pub Vec<Sub>);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Sub {
+    pub is_final: bool,
+    pub supers: Vec<super::TypeIndex>,
+    pub ty: Composite,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Composite {
+    Fn {
+        params: Vec<Value>,
+        returns: Vec<Value>,
+    },
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Value {
+    Num(Number),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Number {
+    I32,
+}
