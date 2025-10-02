4eb35dd74a4d938367f76a8c5376ae5e2bd7cce9
futsuuu <futsuuu123@gmail.com>
2025-10-02 23:55:36 +0900

    feat: implement `rollup()` and `unroll()` methods

--- src/validation/ty.rs
+++ src/validation/ty.rs
@@ -4,8 +4,36 @@ pub struct Recursive {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct RecursiveInner {
+struct RecursiveInner {
     types: Vec<Sub>,
+}
+
+impl std::ops::Deref for Recursive {
+    type Target = Vec<Sub>;
+    fn deref(&self) -> &Self::Target {
+        &self.inner.deref().types
+    }
+}
+
+impl Recursive {
+    pub fn new(value: Vec<Sub>) -> Self {
+        Self {
+            inner: std::rc::Rc::new(RecursiveInner { types: value }),
+        }
+    }
+
+    pub fn rollup(&self, start_typeidx: usize) -> Vec<Defined> {
+        let start = start_typeidx;
+        let end = start + self.len();
+        let rec = Self::new(self.iter().map(|s| s.rollup(start, end)).collect());
+        (0..self.len())
+            .map(|proj| Defined::new(&rec, proj))
+            .collect()
+    }
+
+    fn unroll(&self) -> Self {
+        Self::new(self.iter().map(|s| s.unroll(self)).collect())
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -14,10 +42,32 @@ pub struct Defined {
     proj: usize,
 }
 
+impl Defined {
+    pub fn new(rec: &Recursive, proj: usize) -> Self {
+        Self {
+            rec: rec.clone(),
+            proj,
+        }
+    }
+
+    pub fn unroll(&self) -> Sub {
+        self.rec.unroll()[self.proj].clone()
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct DefinedRef {
     rec: std::rc::Weak<RecursiveInner>,
     proj: usize,
+}
+
+impl DefinedRef {
+    fn new(rec: &Recursive, proj: usize) -> Self {
+        Self {
+            rec: std::rc::Rc::downgrade(&rec.inner),
+            proj,
+        }
+    }
 }
 
 impl DefinedRef {
@@ -45,6 +95,22 @@ pub enum TypeUse {
     Def(DefinedRef),
 }
 
+impl TypeUse {
+    fn rollup(&self, start: usize, end: usize) -> Self {
+        match self {
+            Self::TypeIdx(i) if (start..end).contains(i) => Self::RecTypeIdx(i - start),
+            other => other.clone(),
+        }
+    }
+
+    fn unroll(&self, rec: &Recursive) -> Self {
+        match self {
+            Self::RecTypeIdx(i) => Self::Def(DefinedRef::new(rec, *i)),
+            other => other.clone(),
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Sub {
     pub is_final: bool,
@@ -52,9 +118,41 @@ pub struct Sub {
     pub body: Composite,
 }
 
+impl Sub {
+    fn rollup(&self, start: usize, end: usize) -> Self {
+        Self {
+            is_final: self.is_final,
+            supers: self.supers.iter().map(|u| u.rollup(start, end)).collect(),
+            body: self.body.rollup(start, end),
+        }
+    }
+
+    fn unroll(&self, rec: &Recursive) -> Self {
+        Self {
+            is_final: self.is_final,
+            supers: self.supers.iter().map(|u| u.unroll(rec)).collect(),
+            body: self.body.unroll(rec),
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Composite {
     Func(Func),
+}
+
+impl Composite {
+    fn rollup(&self, start: usize, end: usize) -> Self {
+        match self {
+            Self::Func(f) => Self::Func(f.rollup(start, end)),
+        }
+    }
+
+    fn unroll(&self, rec: &Recursive) -> Self {
+        match self {
+            Self::Func(f) => Self::Func(f.unroll(rec)),
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -63,10 +161,38 @@ pub struct Func {
     returns: Vec<Value>,
 }
 
+impl Func {
+    fn rollup(&self, start: usize, end: usize) -> Self {
+        Self {
+            params: self.params.iter().map(|v| v.rollup(start, end)).collect(),
+            returns: self.returns.iter().map(|v| v.rollup(start, end)).collect(),
+        }
+    }
+
+    fn unroll(&self, rec: &Recursive) -> Self {
+        Self {
+            params: self.params.iter().map(|v| v.unroll(rec)).collect(),
+            returns: self.returns.iter().map(|v| v.unroll(rec)).collect(),
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Value {
     Num(Number),
     Bottom,
+}
+
+impl Value {
+    fn rollup(&self, start: usize, end: usize) -> Self {
+        _ = (start, end);
+        self.clone()
+    }
+
+    fn unroll(&self, rec: &Recursive) -> Self {
+        _ = rec;
+        self.clone()
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
